fdf896fa95b5e0867656474673a327de19877575
KorolPaul <korol.paul@gmail.com>
2015-12-06 22:06:08 +0300

    all pages

--- js/main.js
+++ js/main.js
@@ -1,29 +1,20 @@
 $(function () {
-    
-    /*BXSLIDER*/
-    $(".slider").bxSlider({
-        slideWidth: 500,    //ширина слайда
-        slideMargin: 0,     //расстояние между слайдами
-        mode: 'horizontal', //'horizontal', 'vertical', 'fade' - переход
-        speed: 500,         //ну понятно
-        pager: false,       //пэйджинатор
-        randomStart: false, //рандомный 1-й слайд
-        auto: false,         //тоже понятно
-        pause: 4000,        //ппауза между сменой слайдера
-        minSlides: 1,
-        maxSlides: 1,
-        moveSlides: 2      //сколько слайдов за раз двигать
-    });
 
     /*TABS*/
-    $(".tab-menu a").click(function () {
-        var tab = $(this).parent().index() + 1;
-        $(".tab-menu a").removeClass("active");
-        $(this).addClass("active");
-        $(".tabs>div").fadeOut(500, function () {
-            $(".tabs div.tab-" + tab + "").fadeIn(500);
+    $(".tabs_link").click(function (e) {
+        e.preventDefault();
+        var tab = $(this).index() + 1;
+        $(".tabs_link").removeClass("tabs_link__active");
+        $(this).addClass(".tabs_link__active");
+
+        $(".tabs_content").fadeOut(500, function () {
+            $(".tabs_content:nth("+tab+")").fadeIn(500);
         })
     });
 
-   
+    $('.slider').bxSlider({
+        mode: 'vertical',
+        pager: false,
+        slideMargin: 5
+    });
 });
